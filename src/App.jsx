8ea5e1ea9ca1b17e2443ad74662f9ee7c620be51
Joaquin Sadoc <joakom41@gmail.com>
2025-06-30 16:27:28 -0400

    Api Pokemon

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import PokemonFetcher from './PokemonFetcher.jsx';
+
+function App(){
+    return(
+    <>
+        <h1> !Conoce a tus Pokemon!
+        <PokemonFetcher/>
+        </h1>
+    </>
+    )
+    }
+export default App;
